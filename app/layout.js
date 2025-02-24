2083531ba04f1c2f5202ade13d614f79acec2a22
asif ahemmed <asifahemmed0@gmail.com>
2025-02-24 12:22:58 +0600

    Landing page created

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from '@clerk/themes'
 import Header from "@/components/Header";
 
 const inter = Inter({
@@ -16,7 +17,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider  appearance={{
+      baseTheme: dark,
+    }}>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className}`}
